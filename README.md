e308658843084f1f06e7ee7e11e8d2f4a388dad7
Yujing Wei <jing@YujingdeMacBook-Pro.local>
2025-01-24 11:37:36 -0800

    Debugging alohaWorld and adding greeting class

--- README.md
+++ README.md
@@ -1,22 +1,21 @@
 **Name:**
-
+Yu-Jing Wei<br>
 **Github Account name:**
-
-**Link to Assignment on Github:** (copy and paste the link to your assignment repo here)
-
-How many hours did it take you to complete this assignment (estimate)?
-
+egsui<br>
+**Link to Assignment on Github:** (copy and paste the link to your assignment repo here)<br>
+https://github.com/5004-SEA-Fa24-Geeng/hello-world-egsui/tree/main<br>
+How many hours did it take you to complete this assignment (estimate)? 3hr<br>
 Did you collaborate with any other students/TAs/Professors? If so, tell us who and in what
 capacity.
 
-* one per row, add more if needed
+* Claire Liu
   
 Did you use any external resources (you do not have to cite in class material)? (Cite them below)
 
-* one row per resource
+* draw.io
 
 
-(Optional) What was your favorite part of the assignment?
+(Optional) What was your favorite part of the assignment? Drawing UML diagram.
 
 (Optional) How would you improve the assignment?
 
--- src/main/java/student/Greeting.java
+++ src/main/java/student/Greeting.java
@@ -7,6 +7,62 @@ package student;
  * 
  */
 public class Greeting {
+    private int localityID;
+    private String localityName;
+    private String asciiGreeting = "Hello";
+    private String unicodeGreeting = "Hello";
+    private String formatStr = "%s, %%s!";
 
+    public Greeting(int localityID, String localityName) {
+        this.localityID = localityID;
+        this.localityName = localityName;
+    }
+
+    public Greeting(int localityID, String localityName, String greeting) {
+        this.localityID = localityID;
+        this.localityName = localityName;
+        this.asciiGreeting = greeting;
+        this.unicodeGreeting = greeting;
+    }
+
+    public Greeting(int localityID, String localityName, String asciiGreeting, String unicodeGreeting, String formatStr) {
+        this.localityID = localityID;
+        this.localityName = localityName;
+        this.asciiGreeting = asciiGreeting;
+        this.unicodeGreeting = unicodeGreeting;
+        this.formatStr = formatStr;
+    }
+
+    public int getLocalityID() {
+        return this.localityID;
+    }
+
+    public String getLocalityName() {
+        return this.localityName;
+    }
+
+    public String getAsciiGreeting() {
+        return this.asciiGreeting;
+    }
+
+    public String getUnicodeGreeting() {
+        return this.unicodeGreeting;
+    }
+
+    public String getFormatStr() {
+        return String.format(this.formatStr, this.unicodeGreeting);
+    }
+
+    public String getFormatStr(boolean asciiOnly) {
+        if (asciiOnly) {
+            return String.format(this.formatStr, this.asciiGreeting);
+        }
+        return String.format(this.formatStr, this.unicodeGreeting);
+    }
+
+    public String toString() {
+        return "{localityID:" + this.localityID + ", localityName:\"" + this.localityName +
+                "\", asciiGreeting:\"" + this.asciiGreeting + "\", unicodeGreeting:\"" + this.unicodeGreeting +"\"}";
+    }
 
 }
